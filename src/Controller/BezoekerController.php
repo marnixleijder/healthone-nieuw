fb9018d6c30dbc0b66db8a1843f986bdac9d5869
rvdboogaard <302311005@student.rocmondriaan.nl>
2022-01-28 11:30:24 +0100

    bootstrap & nav bar added

---
+++ src/Controller/BezoekerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BezoekerController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('bezoeker/index.html.twig', [
+            'controller_name' => 'BezoekerController',
+        ]);
+    }
+
+    /**
+     * @Route("/bezoeker", name="bezoeker")
+     */
+    public function index(): Response
+    {
+        return $this->render('bezoeker/index.html.twig', [
+            'controller_name' => 'BezoekerController',
+        ]);
+    }
+}
